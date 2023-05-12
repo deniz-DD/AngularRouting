76292e178bf604581bfd819744314a9d65992632
Deniz <deniz_dural@yahoo.com>
2023-05-12 16:25:25 +0200

    deleted unnescarry things

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -25,6 +25,6 @@ export class AuthGuard implements CanActivate , CanActivateChild{
 
 
     canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        return this.canActivate(childRoute , state)
+        return this.canActivate(childRoute , state);
     }
 }
